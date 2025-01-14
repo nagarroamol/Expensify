f1b16d260852123d3ee30412a85f5c1d76dfa09e
Amol Bavannavar <amol.bavannavar@nagarro.com>
2025-01-14 21:42:32 +0530

    Web URL changes

--- Client/src/Services/axiosInstance.js
+++ Client/src/Services/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Set up the Axios instance with default configuration
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5256/api/', 
+  baseURL: 'http://localhost:5000/api/', 
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
